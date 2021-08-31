cefac7dd62ff064b7a79900e599f5d965a18fb9b
DAMAR <60118254+dnbad@users.noreply.github.com>
2021-08-31 13:01:49 -0500

    Update Dammar_callback.m

--- Dammar_callback.m
+++ Dammar_callback.m
@@ -1,5 +1,14 @@
 % Create the function for the ButtonPushedFcn callback
-function Dammar_callback(btn,whichplot,ax)
+function Dammar_callback(btn,ax)
+x = linspace(0,2*pi,100);
+y = zeros(1,length(x));
+for ii=1:length(x)
+    if x(ii) < pi
+        y(ii) = sin(x(ii));
+    else
+        y(ii) = -sin(x(ii));
+    end
+end
 
-
+plot(ax,x,y,'g')
 end
