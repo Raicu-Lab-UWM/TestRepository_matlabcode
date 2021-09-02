aecad9875b9afba5d21ebceac114f95542b8dedb
JustinT12 <trujil24@uwm.edu>
2021-09-01 20:51:27 -0500

    Justin added a button

--- Justin_callback.m
+++ Justin_callback.m
@@ -1,5 +1,16 @@
 % Create the function for the ButtonPushedFcn callback
 function Justin_callback(btn,whichplot,ax)
 
+x = linspace(0,2*pi,100);
 
+switch whichplot
+    case 0
+        y = 0.005*x + 0.1;
+    
+    otherwise
+        disp('Other value')
+
+end
+C = 'g'; % Color of line
+plot(ax,x,y,'color',C)
 end
