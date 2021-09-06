caca6921cf9a58493346fdfc7a599430c93e7b19
askhan43 <khan43@uwm.edu>
2021-09-05 20:12:01 -0500

    Aliyah_button
    Button added to Main window

--- Aaliyah_callback.m
+++ Aaliyah_callback.m
@@ -1,5 +1,11 @@
 % Create the function for the ButtonPushedFcn callback
 function Aaliyah_callback(btn,whichplot,ax)
 
+t = linspace(0, 2*pi, 100);
+
+x = sin(2*t);
+y = sin(3*t);
+
+plot(ax,x,y,'b')
 
 end
