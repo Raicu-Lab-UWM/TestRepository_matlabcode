5e494bfad60a80bd818746cc5da393a1ef024dd9
dhrubaad <88337270+dhrubaad@users.noreply.github.com>
2021-09-06 22:52:52 -0500

    Update Dhruba_callback.m

--- Dhruba_callback.m
+++ Dhruba_callback.m
@@ -1,5 +1,18 @@
 % Create the function for the ButtonPushedFcn callback
-function Dhruba_callback(btn,whichplot,ax)
+function Mike_callback(btn,whichplot,ax)
 
+x = linspace(0,6*pi,1000);
 
+switch whichplot
+    case 0
+        y = sin(x);
+    case 1
+        y = cos(x);  
+    case 2
+        y = square(x);   
+    otherwise
+        disp('other value')
+end
+C = {'b','r','g'}; % Cell array of colros.
+plot(ax,x,y,'color',C{whichplot+1})
 end
